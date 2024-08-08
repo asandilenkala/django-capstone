87965322a2aabd7d6b9b41cf3ed29c14878a2132
asandilenkala <asandilenkala@gmail.com>
2024-08-08 18:57:53 +0200

    commit and push

---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. Capstone Project - Django documentation master file, created by
+   sphinx-quickstart on Thu Aug  8 17:53:05 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Capstone Project - Django documentation
+=======================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
--- portfolio/apps.py
+++ portfolio/apps.py
@@ -2,5 +2,16 @@ from django.apps import AppConfig
 
 
 class PortfolioConfig(AppConfig):
+    """
+    Configuration class for the 'portfolio' app in the Django project.
+
+    This class is responsible for setting up and configuring the 'portfolio'
+    application. It defines the default primary key field type to use for
+    models in this app, as well as the name of the app.
+
+    Attributes:
+        default_auto_field (str): Specifies the type of primary key to use for models.
+        name (str): The name of the Django application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio'
