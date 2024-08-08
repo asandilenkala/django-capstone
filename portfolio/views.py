b832165d006fdac7178b050e9cce884c9a06daaa
asandilenkala <asandilenkala@gmail.com>
2024-08-08 17:37:59 +0200

    Added docstring to <views.py>

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,17 +7,56 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def home(request):
+    """
+    Renders the home page of the portfolio website.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered home page.
+    """
     return render(request, 'portfolio/home.html')
 
-#@login_required
+
+# @login_required
 def projects(request):
+    """
+    Renders the projects page with a list of all projects.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered projects page with the list of projects.
+    """
     projects = Project.objects.all()
     return render(request, 'portfolio/projects.html', {'projects': projects})
 
+
 def about(request):
+    """
+    Renders the about page of the portfolio website.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered about page.
+    """
     return render(request, 'portfolio/about.html')
 
+
 def user_login(request):
+    """
+    Handles user login functionality. Authenticates the user and logs them in.
+
+    Args:
+        request: The HTTP request object. It should contain the username and password if it's a POST request.
+
+    Returns:
+        HttpResponse: Redirects to the home page if login is successful, otherwise renders the login page again.
+    """
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -27,7 +66,17 @@ def user_login(request):
             return redirect('home')
     return render(request, 'portfolio/login.html')
 
+
 def user_logout(request):
+    """
+    Logs out the current user and redirects to the home page.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: Redirects to the home page after logout.
+    """
     logout(request)
-    #return render(request, 'portfolio/login.html')
+    # return render(request, 'portfolio/login.html')
     return redirect('home')
