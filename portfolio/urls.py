5c6136fb25bc06fb751e3c1f82658f0dc75c1723
asandilenkala <asandilenkala@gmail.com>
2024-08-08 17:37:46 +0200

    Added docstring to <urls.py>

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+# URL patterns for routing different views in the Django application.
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('projects/', views.projects, name='projects'),
-    path('about/', views.about, name='about'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('', views.home, name='home'),  # Route for the homepage view.
+    path('projects/', views.projects, name='projects'),  # Route for the projects page view.
+    path('about/', views.about, name='about'),  # Route for the about page view.
+    path('login/', views.user_login, name='login'),  # Route for the user login view.
+    path('logout/', views.user_logout, name='logout'),  # Route for the user logout view.
 ]
