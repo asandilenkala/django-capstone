574b2977494bba324101ba71e876ba983c4f3d6c
asandilenkala <asandilenkala@gmail.com>
2024-08-08 17:38:43 +0200

    Added docstring to <admin.py>

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Project
 
 # Register your models here.
+# This allows the Project model to be managed through the Django admin interface.
 admin.site.register(Project)
